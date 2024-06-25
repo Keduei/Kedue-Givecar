407046bd6f4798fb593bce5e5b0e60231d5cf63f
Keduei <moonrptr@gmail.com>
2024-06-25 04:45:38 +0300

    Add files via upload

---
+++ Kedue-Givecar/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Kedue'
+description 'MoonRP Icin Yapilmis Givecar'
+version '1.0.0'
+
+server_script {
+	'server.lua'
+}
+
+client_scripts {
+	'client.lua'
+}
